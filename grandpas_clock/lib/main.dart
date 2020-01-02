722e601572aa931d5bd51b9350d2974be286a31e
gondaimgano <gondaimgano@gmail.com>
2020-01-02 16:19:56 +0200

    changes

--- grandpas_clock/lib/main.dart
+++ grandpas_clock/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 import 'package:provider/provider.dart';
 
@@ -141,8 +141,9 @@ class _ClockFaceState extends State<ClockFace>
                             _controller.fling(velocity: iscomplete ? -2 : 2);
                           }
                         }),
-                    style: GoogleFonts.pressStart2P(
-                        fontSize: 25, textStyle: TextStyle(color: accentColor)),
+
+                    style: TextStyle(fontSize: 40,color: accentColor),
+
                   ),
                   SizedBox(
                     height: 12,
